6156723ebf2d0da3718e033a42a07a70346ba569
John Moore <john.moore@nationalarchives.gov.uk>
2022-11-15 15:17:00 +0000

    extracting the strings

--- src/annotation.py
+++ src/annotation.py
@@ -9,4 +9,10 @@ class Annotation:
         dict = xmltodict.parse(xml)
         jsonpath_expression = parse('alto.Layout.Page.PrintSpace.ComposedBlock.TextBlock')
         lis = [match.value for match in jsonpath_expression.find(dict)]
-        print(lis)
+        dict = lis[0]
+        jsonpath_expression = parse('[*].TextLine.[*].String.[*].@CONTENT')
+        lis = [match.value for match in jsonpath_expression.find(dict)]
+        content = ' '.join(lis)
+        print(content)
+
+        
