2d7415a06a1e69063df3510d7effd05fbb99bbd8
John Moore <john.moore@nationalarchives.gov.uk>
2022-11-15 16:20:48 +0000

    extract strings per textblock

--- src/annotation.py
+++ src/annotation.py
@@ -9,10 +9,11 @@ class Annotation:
         dict = xmltodict.parse(xml)
         jsonpath_expression = parse('alto.Layout.Page.PrintSpace.ComposedBlock.TextBlock')
         lis = [match.value for match in jsonpath_expression.find(dict)]
-        dict = lis[0]
-        jsonpath_expression = parse('[*].TextLine.[*].String.[*].@CONTENT')
-        lis = [match.value for match in jsonpath_expression.find(dict)]
-        content = ' '.join(lis)
-        print(content)
+        dict = lis[0] # match captured result in its own list
+        for textline in dict:
+            jsonpath_expression = parse('TextLine.[*].String.[*].@CONTENT')
+            lis = [match.value for match in jsonpath_expression.find(textline)]
+            content = ' '.join(lis)
+            print(content)
 
         
