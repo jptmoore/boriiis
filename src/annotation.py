160d348a3382cfb38c08cefa779e59a7769dd600
John Moore <jmoore@zedstar.org>
2022-11-15 22:14:29 +0000

    make the pattern matching more robust

--- src/annotation.py
+++ src/annotation.py
@@ -7,11 +7,10 @@ class Annotation:
 
     def add(self, xml):
         dict = xmltodict.parse(xml)
-        jsonpath_expression = parse('alto.Layout.Page.PrintSpace.ComposedBlock.TextBlock')
-        lis = [match.value for match in jsonpath_expression.find(dict)]
-        dict = lis[0] # match captured result in its own list
-        for textline in dict:
-            jsonpath_expression = parse('TextLine.[*].String.[*].@CONTENT')
+        jsonpath_expression = parse('alto.Layout.Page.PrintSpace.ComposedBlock[*].TextBlock[*]')
+        textblock = [match.value for match in jsonpath_expression.find(dict)]
+        for textline in textblock:
+            jsonpath_expression = parse('TextLine[*].String[*].@CONTENT')
             lis = [match.value for match in jsonpath_expression.find(textline)]
             content = ' '.join(lis)
             print(content)
