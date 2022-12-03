5ff4a2530d03eeb5cc3cbdb3c16d90b7b2ea02c8
John Moore <jmoore@zedstar.org>
2022-12-03 11:15:52 +0000

    useful for running locally

--- src/repository.py
+++ src/repository.py
@@ -1,11 +1,25 @@
 from git import Repo
+import shutil
+import os
+
 
 class Repository:
     def __init__(self, ctx):
         self.local_repo = ctx.local_repo
         self.remote_repo = ctx.remote_repo
 
+    def __remove__(self):
+        if os.path.exists(self.local_repo):
+            try:
+                shutil.rmtree(self.local_repo)
+            except Exception as e:
+                print(e)
+                return None
+            else:
+                pass
+
     def clone(self):
+        self.__remove__()
         try:
             repo = Repo.clone_from(self.remote_repo, self.local_repo)
             assert repo.__class__ is Repo
